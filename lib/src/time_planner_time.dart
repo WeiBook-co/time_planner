b857f523d8e556d1c6c145f0d8be8ff9de7b1d4e
developerhincapie <julioandres_29@hotmail.es>
2023-03-26 23:23:16 -0500

    Update time_planner_time.dart

--- lib/src/time_planner_time.dart
+++ lib/src/time_planner_time.dart
@@ -18,10 +18,12 @@ class TimePlannerTime extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: config.cellHeight!.toDouble() - 1,
-      width: 60,
+      width: 65,
       child: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 0.0, vertical: 0.0),
-        child: setTimeOnAxis! ? Text(time!) : Center(child: Text(time!)),
+        child: setTimeOnAxis!
+            ? Text(time!)
+            : Center(child: Text(time!, style: const TextStyle(fontSize: 12))),
       ),
     );
   }
